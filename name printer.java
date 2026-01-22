875a1d74b0770ac222e6fbe69f714f9b5662460a
rupathrashri2004-lang <rupathrashri2004@gmail.com>
2026-01-22 13:26:54 +0530

    Create name printer.java

---
+++ name printer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+import java.util.*;
+
+public class name printer {
+
+    public static void main(String[] args) {
+
+
+        
+       Scanner sc=new Scanner (System.in);
+        String name=sc.nextLine();//sanjay
+        System. out.println("Hello\n"+name);
+
+    }
+
+}
