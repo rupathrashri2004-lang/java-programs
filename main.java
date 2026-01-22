9d3eccd1e3d839ba1e24735fb213a5d54b0bcf68
rupathrashri2004-lang <rupathrashri2004@gmail.com>
2026-01-22 13:27:34 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int intNum = sc.nextInt();
+
+        float floatNum = sc.nextFloat();
+
+        System.out.println(intNum);
+
+        System.out.printf("%.2f", floatNum);
+
+    }
+
+}
